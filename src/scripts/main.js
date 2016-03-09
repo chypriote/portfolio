7c3f26527a45f532a609c64225708135a6b3c827
ChypRiotE <nicolastem@hotmail.fr>
2016-03-09 17:38:53 +0800

    mep

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,80 +1,80 @@
-$(document).ready(function() {
-	$('.open-menu').click(function(){
-		$('.navigation').removeClass('navigation-slim');
-		$('.sub-bar').slideToggle('slow');
-	});
-	$('.mobile.menu a').click(function(){
-		$('.sub-bar').slideToggle('slow');
-	});
-	//Sticky header
-		$(window).scroll(function() {
-			if ($(this).scrollTop() > 1){
-				$('.navigation').addClass("navigation-slim");
-			} else{
-				$('.navigation').removeClass("navigation-slim");
-			}
-		});
-		$(function() {
-			$('nav a[href*=#]:not([href=#])').click(function() {
-				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-					var target = $(this.hash);
-					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-					if (target.length) {
-						$('html,body').animate({
-							scrollTop: target.offset().top
-						}, 1000);
-						return false;
-					}
-				}
-			});
-		});
-
-	//Empeche le click random dans les skills
-		$('.skill').click(function(){
-			$(this).toggleClass('rectangle');
-		});
-
-	//Affichage d'un projet
-		$('.closeicon').click(function(){
-			$('#work').slideUp();
-		});
-		$('.ih-item').click(function(){
-			$('#work').fadeIn();
-			$('#work .work-img').attr('src', $(this).attr('data-img'));
-			$('#work .work-title').html($(this).attr('data-title'));
-			$('#work .work-description').html($(this).attr('data-description'));
-			$('#work .work-skills').html($(this).find('.project-skills').html());
-			if ($(this).attr('data-link')) {
-				$('#work .work-site').attr('href', $(this).attr('data-link')).show();
-			} else {
-				$('#work .work-site').hide();
-			}
-			if ($(this).attr('data-git')) {
-				$('#work .work-git').attr('href', $(this).attr('data-git')).show();
-			} else {
-				$('#work .work-git').hide();
-			}
-		});
-
-		$('.project-slider').slick({
-			prevArrow: '',
-			nextArrow: '<a class="project-next">Voir plus</a>',
-			slidesToShow: 3,
-			slidesToScroll: 1,
-			responsive: [
-				{
-					breakpoint: 992,
-					settings: {slidesToShow: 2}
-				},
-				{
-					breakpoint: 768,
-					settings: {slidesToShow: 2}
-				}
-			]
-		});
-
-	//Masquer skills
-		$('.skill-hide').click(function(){
-			$('.not-yet').toggle();
-		});
+$('.open-menu').click(function(){
+	$('.navigation').removeClass('navigation-slim');
+	$('.sub-bar').slideToggle('slow');
 });
+$('.mobile.menu a').click(function(){
+	$('.sub-bar').slideToggle('slow');
+});
+//Sticky header
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1){
+			$('.navigation').addClass("navigation-slim");
+		} else{
+			$('.navigation').removeClass("navigation-slim");
+		}
+	});
+	$(function() {
+		$('nav a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
+
+//Empeche le click random dans les skills
+	$('.skill').click(function(){
+		$(this).toggleClass('rectangle');
+	});
+
+//Affichage d'un projet
+	$('.closeicon').click(function(){
+		$('#work').slideUp();
+	});
+	$('.ih-item').click(function(){
+		$('#work').fadeIn();
+		$('#work .work-img').attr('src', $(this).attr('data-img'));
+		$('#work .work-title').html($(this).attr('data-title'));
+		$('#work .work-description').html($(this).attr('data-description'));
+		$('#work .work-skills').html($(this).find('.project-skills').html());
+		if ($(this).attr('data-link')) {
+			$('#work .work-site').attr('href', $(this).attr('data-link')).show();
+		} else {
+			$('#work .work-site').hide();
+		}
+		if ($(this).attr('data-git')) {
+			$('#work .work-git').attr('href', $(this).attr('data-git')).show();
+		} else {
+			$('#work .work-git').hide();
+		}
+	});
+
+	$('.project-slider').slick({
+		prevArrow: '',
+		nextArrow: '<a class="project-next">Voir plus</a>',
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {slidesToShow: 2}
+			},
+			{
+				breakpoint: 768,
+				settings: {slidesToShow: 2}
+			}
+		]
+	});
+
+//Masquer skills
+	$('.skill-hide').click(function(){
+		$('.not-yet').toggle();
+	});
+
+$('img').unveil({offset:200});
