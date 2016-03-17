387f4f2b69fd79d7fb52502ccd606500abbebefd
ChypRiotE <nicolastem@hotmail.fr>
2016-03-17 23:13:05 +0800

    clean up typography

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,16 +14,14 @@ $('.mobile.menu a').click(function(){
 		}
 	});
 	$(function() {
-		$('nav a[href*=#]:not([href=#])').click(function() {
-			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-				if (target.length) {
-					$('html,body').animate({
-						scrollTop: target.offset().top
-					}, 1000);
-					return false;
-				}
+		$('nav a').click(function() {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
 			}
 		});
 	});
